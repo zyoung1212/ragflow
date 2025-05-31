abde7fa3872f13d5a37f808ddd92f9d46feedc6f
何子洋 <hzy@singhand.com>
2025-06-01 01:27:50 +0800

    update

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -4,7 +4,7 @@ import os
 
 # Server socket
 bind = f"{os.environ.get('RAGFLOW_HOST_IP', '0.0.0.0')}:{os.environ.get('RAGFLOW_HOST_PORT', '9380')}"
-backlog = 2048
+backlog = 20480
 
 # Worker processes
 workers = int(os.environ.get('GUNICORN_WORKERS', min(multiprocessing.cpu_count() * 2 + 1, 8)))

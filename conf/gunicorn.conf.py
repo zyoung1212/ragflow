dbd5f93bcca17b910fe3a65280588e43366b9394
何子洋 <hzy@singhand.com>
2025-06-01 14:12:46 +0800

    update

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -22,7 +22,7 @@ preload_app = True
 # Logging
 accesslog = '-'
 errorlog = '-'
-loglevel = 'info'
+loglevel = 'debug'
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
 # Process naming

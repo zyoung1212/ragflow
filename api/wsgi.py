5e72e25c835075e4a7ac6796ef8c591f4f952374
hzy <zyoung1212@gmail.com>
2025-06-02 18:22:38 +0800

    1

--- api/wsgi.py
+++ api/wsgi.py
@@ -19,6 +19,13 @@ import os
 if os.environ.get('GUNICORN_WORKER_CLASS') == 'gevent':
     from gevent import monkey
     monkey.patch_all()
+    
+    # Import gevent for greenlet spawning
+    import gevent
+    from gevent import spawn
+    USE_GEVENT = True
+else:
+    USE_GEVENT = False
 
 from api.utils.log_utils import initRootLogger
 from plugin import GlobalPluginManager
@@ -27,17 +34,11 @@ from plugin import GlobalPluginManager
 initRootLogger("ragflow_server")
 
 import logging
-import os
 import signal
 import threading
+import time
 import uuid
-try:
-    # Try to use gevent's thread pool if available
-    from gevent.threadpool import ThreadPool
-    USE_GEVENT_THREADPOOL = True
-except ImportError:
-    from concurrent.futures import ThreadPoolExecutor
-    USE_GEVENT_THREADPOOL = False
+from concurrent.futures import ThreadPoolExecutor
 
 from api import settings
 from api.apps import app
@@ -52,9 +53,15 @@ from api.utils import show_configs
 from rag.settings import print_rag_settings
 from rag.utils.redis_conn import RedisDistributedLock
 
-# Global stop event and executor for background tasks
-stop_event = threading.Event()
-background_executor = None
+# Global variables for background tasks
+if USE_GEVENT:
+    stop_event = None
+    background_executor = None
+    background_greenlet = None
+else:
+    stop_event = threading.Event()
+    background_executor = None
+    background_greenlet = None
 
 RAGFLOW_DEBUGPY_LISTEN = int(os.environ.get('RAGFLOW_DEBUGPY_LISTEN', "0"))
 
@@ -64,27 +71,50 @@ def update_progress():
     lock_value = str(uuid.uuid4())
     redis_lock = RedisDistributedLock("update_progress", lock_value=lock_value, timeout=60)
     logging.info(f"update_progress lock_value: {lock_value}")
-    while not stop_event.is_set():
-        try:
-            if redis_lock.acquire():
-                DocumentService.update_progress()
+    
+    if USE_GEVENT:
+        # Use gevent sleep and loop for greenlet compatibility
+        while True:
+            try:
+                if redis_lock.acquire():
+                    DocumentService.update_progress()
+                    redis_lock.release()
+                gevent.sleep(6)  # Use gevent.sleep instead of stop_event.wait
+            except Exception:
+                logging.exception("update_progress exception")
                 redis_lock.release()
-            stop_event.wait(6)
-        except Exception:
-            logging.exception("update_progress exception")
-        finally:
-            redis_lock.release()
+                break
+    else:
+        # Traditional threading approach
+        while not stop_event.is_set():
+            try:
+                if redis_lock.acquire():
+                    DocumentService.update_progress()
+                    redis_lock.release()
+                stop_event.wait(6)
+            except Exception:
+                logging.exception("update_progress exception")
+            finally:
+                redis_lock.release()
 
 
 def signal_handler(sig, frame):
     """Handle shutdown signals gracefully"""
-    logging.info("Received interrupt signal, shutting down...")
-    stop_event.set()
-    if background_executor:
-        if USE_GEVENT_THREADPOOL:
-            background_executor.kill()
-        else:
+    logging.info("Received shutdown signal, stopping background tasks...")
+    
+    if USE_GEVENT:
+        # Kill the background greenlet
+        global background_greenlet
+        if background_greenlet and not background_greenlet.dead:
+            background_greenlet.kill()
+    else:
+        # Traditional threading approach
+        stop_event.set()
+        if hasattr(background_executor, 'shutdown'):
             background_executor.shutdown(wait=False)
+    
+    logging.info("Background tasks stopped")
+    exit(0)
 
 
 def initialize_ragflow():
@@ -128,12 +158,16 @@ def initialize_ragflow():
     signal.signal(signal.SIGTERM, signal_handler)
 
     # Start background progress update task
-    if USE_GEVENT_THREADPOOL:
-        background_executor = ThreadPool(maxsize=1)
-        background_executor.spawn(update_progress)
+    if USE_GEVENT:
+        # Use gevent spawn for greenlet-based execution
+        global background_greenlet
+        background_greenlet = spawn(update_progress)
+        logging.info("Started document progress update task in gevent mode")
     else:
+        # Use thread pool for traditional threading
         background_executor = ThreadPoolExecutor(max_workers=1)
         background_executor.submit(update_progress)
+        logging.info("Started document progress update task in threading mode")
 
     logging.info("RAGFlow WSGI application initialized successfully in production mode")
 
